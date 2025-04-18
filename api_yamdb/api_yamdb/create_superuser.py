7b81a9e8889de62937777d924e02ad8938fff512
user <user@Sashas-MacBook-Pro.local>
2025-04-18 16:20:31 +0300

    superuser1

--- api_yamdb/api_yamdb/create_superuser.py
+++ api_yamdb/api_yamdb/create_superuser.py
@@ -1,5 +1,3 @@
-# api_yamdb/create_superuser.py
-
 import os
 import django
 
@@ -10,12 +8,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminadmin')
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser {username} created ✅")
-else:
-    print(f"Superuser {username} already exists ✅")
+if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
+    User.objects.create_superuser(
+        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
