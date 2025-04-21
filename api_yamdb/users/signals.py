40c7cf4ef237099dfd3b7c9a8a5bfd0e15640a93
user <user@Sashas-MacBook-Pro.local>
2025-04-21 13:37:00 +0300

    render deploy db add superuser3

--- api_yamdb/users/signals.py
+++ api_yamdb/users/signals.py
@@ -8,9 +8,9 @@ User = get_user_model()
 
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
-    username = os.getenv('DJANGO_SUPERUSER_USERNAME')
-    email = os.getenv('DJANGO_SUPERUSER_EMAIL')
-    password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminadmin')
 
     if username and email and password:
         if not User.objects.filter(username=username).exists():
