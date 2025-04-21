a0caebbb758e550fd1ccb78d071e3cdbed082f6b
user <user@Sashas-MacBook-Pro.local>
2025-04-21 14:21:35 +0300

    opt dockerfile3

--- api_yamdb/api_yamdb/create_superuser.py
+++ api_yamdb/api_yamdb/create_superuser.py
@@ -10,8 +10,8 @@ User = get_user_model()
 
 if not User.objects.filter(
     username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
-    User.objects.create_superuser(
-        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        User.objects.create_superuser(
+            username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+            email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+            password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
     )
